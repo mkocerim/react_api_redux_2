1f9a95ff734536dfd9ff740f2792c42f1f88d83a
Mustafa Koçer <mkocerim@gmail.com>
2023-04-24 23:55:31 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,74 +1,64 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { connect } from "react-redux";
 
-
-
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Home from './pages/home/home';
-import Login from '../src/pages/login/login'
-import Categories from '../src/pages/categories/categories'
-import Services from '../src/pages/services/services'
-import Register from './pages/register/register';
-import useApi from './Hooks/useApi';
-import { SET_CATEGORIES } from './redux/reducers/categoriesReducer/categoriesReducer';
-
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home/home";
+import Login from "../src/pages/login/login";
+import Categories from "../src/pages/categories/categories";
+import Services from "../src/pages/services/services";
+import Register from "./pages/register/register";
+import useApi from "./Hooks/useApi";
+import { SET_CATEGORIES } from "./redux/reducers/categoriesReducer/categoriesReducer";
 
 function App(props) {
-  console.log('>> APP PROPS', props) 
-  const api=useApi()
+  console.log(">> APP PROPS", props);
+  const api = useApi();
 
-  if(props.categoriesState.initialized===false){
+  if (props.categoriesState.initialized === false) {
+    api
+      .get(
+        "https://api.adoptez1artisan.com/public/categories/listMainCategories"
+      )
+      .then((response) => {
+        console.log(">>KATEGORI LISTESI CEVAP", response);
 
-  api.get('https://api.adoptez1artisan.com/public/categories/listMainCategories')
-  .then((response)=>{
-    console.log('>>KATEGORI LISTESI CEVAP',response)
-    
-    const action = {
-      type:SET_CATEGORIES,
-      payload: {
-        categories:response.data.data
-      }
-
-    }
-    props.dispatch(action)
-  })
-  .catch((err)=>{
-    console.error('>>KATEGORI LISTESI HATASI',err)
-  })
-}
+        const action = {
+          type: SET_CATEGORIES,
+          payload: {
+            categories: response.data.data,
+          },
+        };
+        props.dispatch(action);
+      })
+      .catch((err) => {
+        console.error(">>KATEGORI LISTESI HATASI", err);
+      });
+  }
 
   return (
     <div className="container py-3">
-
-      <Header/>
+      <Header />
 
       <HashRouter>
         <Routes>
-          <Route index element = {<Home/>}/>
-          <Route  path ="login" element = {<Login/>}/>
-          <Route  path ="register" element = {<Register/>}/>
-          <Route  path ="categories" element = {<Categories/>}/>
-          <Route  path ="category/:slug" element = {<Categories/>}/>
-          <Route  path ="services" element = {<Services/>}/>          
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="category/:slug" element={<Categories />} />
+          <Route path="services" element={<Services />} />
         </Routes>
       </HashRouter>
-  
-      <Footer/>
 
+      <Footer />
     </div>
-
-  )
-    
+  );
 }
-const mapStateToProps=(state)=>{
-
-
-  return{
+const mapStateToProps = (state) => {
+  return {
     ...state,
-
-
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(App);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,31 +1,29 @@
-import BootstrapLogo from '../../svg/bootstrap.svg'
+import BootstrapLogo from "../../svg/bootstrap.svg";
 
-function Header () {
-return(
+function Header() {
+  return (
     <header>
-    <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-      <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
-        
-        <img src={BootstrapLogo}/>
+      <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <img src={BootstrapLogo} />
+          &nbsp;
+          <span className="fs-4">Hizmet Bulma Uygulamasi</span>
+        </a>
 
-        &nbsp;
-
-        <span className="fs-4">Hizmet Bulma Uygulamasi</span>
-      </a>
-
-      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <a href="#/login" className='btn btn-primary me-1'>Login</a>
-        <a href="#/register" className='btn btn-primary me-1'>Register</a>
-
-       
-      </nav>
-    </div>
-
-    
-  </header>
-)
-
-
+        <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+          <a href="#/login" className="btn btn-primary me-1">
+            Login
+          </a>
+          <a href="#/register" className="btn btn-primary me-1">
+            Register
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
